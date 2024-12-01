fadf69f8ac1dd76e3b704707b5c29d64cbb310a8
Clownacy <Clownacy@users.noreply.github.com>
2024-12-01 01:27:40 +0000

    Use frontend-provided file IO functions.

--- libretro.c
+++ libretro.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -75,6 +76,10 @@ static struct
 	retro_input_state_t        input_state;
 	CC_ATTRIBUTE_PRINTF(2, 3) retro_log_printf_t log;
 } libretro_callbacks;
+
+/************************/
+/* ClownMDEmu Callbacks */
+/************************/
 
 static cc_u8f CartridgeReadCallback(void* const user_data, const cc_u32f address)
 {
@@ -374,6 +379,10 @@ static size_t CDAudioReadCallback(void* const user_data, cc_s16l* const sample_b
 	return CDReader_ReadAudio(&cd_reader, sample_buffer, total_frames);
 }
 
+/***********/
+/* Logging */
+/***********/
+
 CC_ATTRIBUTE_PRINTF(2, 3) static void FallbackErrorLogCallback(const enum retro_log_level level, const char* const format, ...)
 {
 	va_list args;
@@ -419,6 +428,10 @@ static void ClownMDEmuLog(void* const user_data, const char* const format, va_li
 
 	libretro_callbacks.log(RETRO_LOG_WARN, "%s", message_buffer);
 }
+
+/***********/
+/* Options */
+/***********/
 
 static cc_bool DoOptionBoolean(const char* const key, const char* const true_value)
 {
@@ -477,8 +490,162 @@ static void UpdateOptions(const cc_bool only_update_flags)
 	clownmdemu_configuration.pcm.channels_disabled[7] = DoOptionBoolean("clownmdemu_disable_pcm8", "enabled");
 }
 
+/***********/
+/* File IO */
+/***********/
+
+static retro_vfs_open_t File_Open;
+static retro_vfs_close_t File_Close;
+static retro_vfs_size_t File_GetSize;
+static retro_vfs_tell_t File_Tell;
+static retro_vfs_seek_t File_Seek;
+static retro_vfs_read_t File_Read;
+static retro_vfs_write_t File_Write;
+
+static struct retro_vfs_file_handle* RETRO_CALLCONV File_OpenDefault(const char* const path, const unsigned int mode, const unsigned int hints)
+{
+	const char* mode_standard;
+
+	(void)hints;
+
+	switch (mode)
+	{
+		case RETRO_VFS_FILE_ACCESS_READ:
+		case RETRO_VFS_FILE_ACCESS_READ | RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING:
+			mode_standard = "rb";
+			break;
+
+		case RETRO_VFS_FILE_ACCESS_WRITE:
+			mode_standard = "wb";
+			break;
+
+		case RETRO_VFS_FILE_ACCESS_WRITE | RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING:
+		case RETRO_VFS_FILE_ACCESS_READ | RETRO_VFS_FILE_ACCESS_WRITE | RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING:
+			mode_standard = "r+b";
+			break;
+
+		case RETRO_VFS_FILE_ACCESS_READ | RETRO_VFS_FILE_ACCESS_WRITE:
+			mode_standard = "w+b";
+			break;
+
+		default:
+			return NULL;
+	}
+
+	return (struct retro_vfs_file_handle*)fopen(path, mode_standard);
+}
+
+static int RETRO_CALLCONV File_CloseDefault(struct retro_vfs_file_handle* const stream)
+{
+	if (stream == NULL)
+		return -1;
+
+	return fclose((FILE*)stream) == 0 ? 0 : -1;
+}
+
+
+static int64_t RETRO_CALLCONV File_GetSizeDefault(struct retro_vfs_file_handle* const stream)
+{
+	FILE* const file = (FILE*)stream;
+	fpos_t position;
+	int64_t result = -1;
+
+	if (fgetpos(file, &position) == 0)
+	{
+		if (fseek(file, 0, SEEK_END) == 0)
+			result = ftell(file);
+
+		if (fsetpos(file, &position) != 0)
+			result = -1;
+	}
+
+	return result;
+}
+
+static int64_t RETRO_CALLCONV File_TellDefault(struct retro_vfs_file_handle* const stream)
+{
+	return ftell((FILE*)stream);
+}
+
+static int64_t RETRO_CALLCONV File_SeekDefault(struct retro_vfs_file_handle* const stream, const int64_t offset, const int seek_position)
+{
+	int whence;
+
+	if (offset < LONG_MIN || offset > LONG_MAX)
+		return -1;
+
+	switch (seek_position)
+	{
+		case RETRO_VFS_SEEK_POSITION_START:
+			whence = SEEK_SET;
+			break;
+
+		case RETRO_VFS_SEEK_POSITION_CURRENT:
+			whence = SEEK_CUR;
+			break;
+
+		case RETRO_VFS_SEEK_POSITION_END:
+			whence = SEEK_END;
+			break;
+
+		default:
+			return -1;
+	}
+
+	if (fseek((FILE*)stream, offset, whence) != 0)
+		return -1;
+
+	return File_TellDefault(stream);
+}
+
+static int64_t RETRO_CALLCONV File_ReadDefault(struct retro_vfs_file_handle* const stream, void* const s, const uint64_t len)
+{
+	if (len > (size_t)-1)
+		return -1;
+
+	return fread(s, 1, len, (FILE*)stream);
+}
+
+static int64_t RETRO_CALLCONV File_WriteDefault(struct retro_vfs_file_handle* const stream, const void* const s, const uint64_t len)
+{
+	if (len > (size_t)-1)
+		return -1;
+
+	return fwrite(s, 1, len, (FILE*)stream);
+}
+
+static void LoadFileIOCallbacks(void)
+{
+	struct retro_vfs_interface_info info;
+
+	info.required_interface_version = 1;
+
+	if (libretro_callbacks.environment(RETRO_ENVIRONMENT_GET_VFS_INTERFACE, &info))
+	{
+		File_Open = info.iface->open;
+		File_Close = info.iface->close;
+		File_GetSize = info.iface->size;
+		File_Tell = info.iface->tell;
+		File_Seek = info.iface->seek;
+		File_Read = info.iface->read;
+		File_Write = info.iface->write;
+	}
+	else
+	{
+		File_Open = File_OpenDefault;
+		File_Close = File_CloseDefault;
+		File_GetSize = File_GetSizeDefault;
+		File_Tell = File_TellDefault;
+		File_Seek = File_SeekDefault;
+		File_Read = File_ReadDefault;
+		File_Write = File_WriteDefault;
+	}
+}
+
 void retro_init(void)
 {
+	LoadFileIOCallbacks();
+
 	/* Inform frontend of serialisation quirks. */
 	{
 	uint64_t serialisation_quirks = RETRO_SERIALIZATION_QUIRK_ENDIAN_DEPENDENT | RETRO_SERIALIZATION_QUIRK_PLATFORM_DEPENDENT;
@@ -770,24 +937,21 @@ bool retro_load_game(const struct retro_game_info* const info)
 		else
 		{
 			/* Mega Drive game. */
-			FILE* const file = fopen(info->path, "rb");
+			struct retro_vfs_file_handle* const file = File_Open(info->path, RETRO_VFS_FILE_ACCESS_READ, RETRO_VFS_FILE_ACCESS_HINT_NONE);
 
 			if (file != NULL)
 			{
-				if (fseek(file, 0, SEEK_END) == 0)
+				const int64_t file_size = File_GetSize(file);
+
+				if (file_size >= 0)
 				{
-					const long position = ftell(file);
+					unsigned char *file_buffer = (unsigned char*)malloc((size_t)file_size);
 
-					if (position >= 0)
+					if (file_buffer != NULL)
 					{
-						const size_t file_size = (size_t)position;
-						unsigned char *file_buffer = (unsigned char*)malloc(file_size);
-
-						if (file_buffer != NULL)
+						if (File_Seek(file, 0, RETRO_VFS_SEEK_POSITION_START) == 0)
 						{
-							rewind(file);
-
-							if (fread(file_buffer, 1, file_size, file) == file_size)
+							if (File_Read(file, file_buffer, file_size) == file_size)
 							{
 								rom = local_rom_buffer = file_buffer;
 								rom_size = file_size;
@@ -795,13 +959,13 @@ bool retro_load_game(const struct retro_game_info* const info)
 
 								success = true;
 							}
-
-							free(file_buffer);
 						}
+
+						free(file_buffer);
 					}
 				}
 
-				fclose(file);
+				File_Close(file);
 			}
 		}
 	}
